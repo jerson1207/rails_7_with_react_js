7870cd67d0c4340a3b47a83aa82291c83622f9b6
jersonjabat <jersonjabatstudent@gmail.com>
2024-08-23 16:57:59 +0800

    Lesson 6: State Hooks

--- app/javascript/react/src/components/QuestionDetail.jsx
+++ app/javascript/react/src/components/QuestionDetail.jsx
@@ -1,7 +1,11 @@
 import * as React from 'react'
+import { useState } from 'react'
 import * as ReactDOM from 'react-dom'
 
 const QuestionDetail = (props) => {
+
+  const [likeCount, setLikeCount] = useState(0)
+
   return(
     <div className="card rounded-0 mt-3">
       <div className="card-body">
@@ -9,6 +13,12 @@ const QuestionDetail = (props) => {
         <p className="lead">
           <div className="badge bg-primary">{props.question.tag}</div>
         </p>
+        <button className="btn btn-primary mt-1" onClick={() => 
+          setLikeCount(likeCount + 1)}>Like</button>
+        { likeCount > 0 ?
+            <span className="badge bg-primary">{likeCount}</span> : ''
+          
+        }
       </div>
     </div>
   )
--- app/javascript/react/src/components/QuestionList.jsx
+++ app/javascript/react/src/components/QuestionList.jsx
@@ -5,42 +5,52 @@ import QuestionDetail from './QuestionDetail'
 const QuestionList = () => {
   const questionList = [
     {
+      id: 1,
       title: 'How to check if a key is present in the Hash?',
       tag: "Ruby"
     },
     {
+      id: 2,
       title: 'What is the difference between `map` and `collect` in Ruby?',
       tag: "Ruby"
     },
     {
+      id: 3,
       title: 'How do you convert a string to a symbol in Ruby?',
       tag: "Ruby"
     },
     {
+      id: 4,
       title: 'What are Ruby blocks, procs, and lambdas?',
       tag: "Ruby"
     },
     {
+      id: 5,
       title: 'How can you handle exceptions in Ruby?',
       tag: "Ruby"
     },
     {
+      id: 6,
       title: 'What is the purpose of the `self` keyword in Ruby?',
       tag: "Ruby"
     },
     {
+      id: 7,
       title: 'How do you define a class in Ruby?',
       tag: "Ruby"
     },
     {
+      id: 8,
       title: 'What is the use of the `attr_accessor` method in Ruby?',
       tag: "Ruby"
     },
     {
+      id: 9,
       title: 'How can you iterate over a hash in Ruby?',
       tag: "Ruby"
     },
     {
+      id: 10,
       title: 'What are Ruby modules and how are they used?',
       tag: "Ruby"
     }
