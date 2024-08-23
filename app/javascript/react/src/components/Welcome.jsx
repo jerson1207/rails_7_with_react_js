8ce360fa823db9703a75f31fe35d2b1c357a3649
jersonjabat <jersonjabatstudent@gmail.com>
2024-08-23 17:16:40 +0800

    Lesson 6: Handling State Class in State Components

--- app/javascript/react/src/components/Welcome.jsx
+++ app/javascript/react/src/components/Welcome.jsx
@@ -2,14 +2,16 @@ import * as React from 'react'
 import * as ReactDOM from 'react-dom/client'
 import QuestionList from './QuestionList'
 
-const Welcome = () => {
-  return(
-    <div className="container">
-      <h1>Hello World! Welcome to the Rails 7 course with React JS</h1>
-      <p>This is the first lesson</p>
-      <QuestionList />
-    </div>
-  )
+class Welcome extends React.Component {
+  render() {
+    return(
+      <div className="container">
+        <h1>Hello World! Welcome to the Rails 7 course with React JS</h1>
+        <p>This is the first lesson</p>
+        <QuestionList />
+      </div>
+    )
+  }
 }
 
 const root = ReactDOM.createRoot(document.getElementById('welcome'))
