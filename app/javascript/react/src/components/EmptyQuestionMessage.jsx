f5faf480f07aaed6103bd5ec4ab0a37307a38f1b
jersonjabat <jersonjabatstudent@gmail.com>
2024-08-23 23:23:03 +0800

    Lesson 10: Dropdown fix error issue

--- app/javascript/react/src/components/EmptyQuestionMessage.jsx
+++ app/javascript/react/src/components/EmptyQuestionMessage.jsx
@@ -3,10 +3,12 @@ import * as ReactDOM from 'react-dom'
 
 const EmptyQuestionMessage = (props) => {
   return(
-    <div className="alert alert-danger alert-dismissible fade show" role="alert">
-      <strong>OOPs!</strong> No Question Found in the tag {props.tagname}.
-      Please select another option from the list.
-      <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    <div>
+      <div className="alert alert-danger alert-dismissible fade show" role="alert">
+        <strong>OOPs!</strong> No Question Found in the tag {props.tagname}.
+        Please select another option from the list.
+        <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+      </div>
     </div>
   )
 }
